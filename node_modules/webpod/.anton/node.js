d92e9a6b54bdbfdd53270e1fd85152e23a972fa0
decentparadox <rsasank69@gmail.com>
2024-01-25 18:48:30 +0530

    bruh first steps

--- node_modules/webpod/.anton/node.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../dist/index.d.ts" />
-import {ssh} from '../dist/index.js'
-
-const $ = ssh('deployer@demo.deployer.org', {port: 22})
-console.log(await $`echo ${"hello world!"} | less`)
