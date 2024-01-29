5eb51a128aada8e26035db2476ab0dca47a36f3f
decentparadox <rsasank69@gmail.com>
2024-01-30 00:36:06 +0530

    Seperation between distro

--- src/install.ts
+++ src/install.ts
@@ -3,8 +3,9 @@ import { setTimeout } from 'node:timers/promises';
 import color from 'picocolors';
 import { exec } from "./exec";
 import { os } from 'zx';
-const { execSync } = require('child_process');
-
+import { execSync } from "child_process";
+import {handleUbuntu}  from "./scripts/ubuntu"
+import {handleArch}  from "./scripts/arch"
 
 
 async function install() {
@@ -13,7 +14,13 @@ async function install() {
 	let osHomeDir = os.homedir();
 	const desktopEnvironment = execSync('echo $XDG_CURRENT_DESKTOP', { encoding: 'utf-8' }).trim();
 	const desktopSession = execSync('echo $DESKTOP_SESSION', { encoding: 'utf-8' }).trim();
-
+	let desktopDistro = execSync("awk -F= '/PRETTY_NAME/ {print $2}' /etc/os-release	", { encoding: 'utf-8' }).trim();
+	if (desktopDistro.toLowerCase().includes("ubuntu")) {
+		desktopDistro = "ubuntu";
+	}
+	if (desktopDistro.toLowerCase().includes("arch")) {
+		desktopDistro = "arch";
+	}
 	if (osPlatform !='linux' ){
 		p.cancel(`De2De: \n Error: Cannot use De2De in ${color.bgRed(color.white(`${osVersion}`))} when you're expected to use it in 'linux' \n `)
 		process.exit(0);
@@ -35,98 +42,12 @@ async function install() {
 
 	p.intro(`${color.bgCyan(color.black(' Kalser '))} ${color.bgCyan(color.black(' v0.0.1 '))}`);
 
-	let SysInfo = `Username: ${os.userInfo().username} \nOS Version: ${osVersion}\nOS Platform: ${osPlatform}\nDesktop Session: ${desktopSession}\nDesktop Environment: ${desktopEnvironment}  `
+	let SysInfo = `Username: ${os.userInfo().username} \nOS Version: ${osVersion}\nOS Platform: ${osPlatform}\nDesktop Session: ${desktopSession}\nDesktop Environment: ${desktopEnvironment} \nDesktop Distro: ${desktopDistro}`
 	p.note(SysInfo, `${color.blue('System Info')}`);
-
-	const project:any = await p.group(
-		{
-			theme: ({ results }) =>
-				p.multiselect({
-						message: `Hey ${os.userInfo().username}, let's start with themes you would like.`,
-						// initialValues: [],
-						options: [
-								{ value: 'gruvbox', label: 'Gruvbox Dark', hint: 'recommended' },
-								{ value: 'everforest', label: 'Everforest Dark', hint: 'recommended' },
-								{ value: 'tokyonight', label: 'Tokyonight Storm' },
-								{ value: 'nightfox', label: 'Nightfox Dusk' },
-								{ value: 'kanagawa', label: 'Kanagawa' },
-								{ value: 'catppuccin', label: 'Catppuccin Mocha' },
-								{ value: 'material', label: 'Material Palenight' },
-								{ value: 'rosepine', label: 'Rosepine Main' },
-							],
-						}),
-				
-				
-			// template: ({results}) =>
-			// 	p.select({
-			// 		message: `Pick a template type within "${results.theme}"`,
-			// 		initialValue: 'blank',
-			// 		maxItems: 5,
-			// 		options: [
-			// 			{ value: 'blank', label: 'Just orca initialized',  hint: 'Recommended (Best for customizing)' },
-			// 			{ value: 'OrcaBackground', label: 'OrcaBackground template' },
-			// 			{ value: 'OrcaFont', label: 'OrcaFont template' },
-			// 			{ value: 'OrcaTextures', label: 'OrcaTextures template' },
-			// 			{ value: 'OrcaSick', label: 'OrcaSick template' },
-			// 			{ value: 'OrcaMinimal', label: 'OrcaMinimal template', hint: 'oh no' },
-			// 		],
-			// 	}),
-			// type: ({ results }) =>
-			// 	p.select({
-			// 		message: `Pick a project type within "${results.path}"`,
-			// 		initialValue: 'ts',
-			// 		maxItems: 5,
-			// 		options: [
-			// 			{ value: 'ts', label: 'TypeScript' },
-			// 			{ value: 'js', label: 'JavaScript' },
-			// 			{ value: 'rust', label: 'Rust' },
-			// 			{ value: 'go', label: 'Go' },
-			// 			{ value: 'python', label: 'Python' },
-			// 			{ value: 'coffee', label: 'CoffeeScript', hint: 'oh no' },
-			// 		],
-			// 	}),
-			// tools: () =>
-			// 	p.multiselect({
-			// 		message: 'Select additional tools.',
-			// 		initialValues: ['prettier', 'eslint'],
-			// 		options: [
-			// 			{ value: 'prettier', label: 'Prettier', hint: 'recommended' },
-			// 			{ value: 'eslint', label: 'ESLint', hint: 'recommended' },
-			// 			{ value: 'stylelint', label: 'Stylelint' },
-			// 			{ value: 'gh-action', label: 'GitHub Action' },
-			// 		],
-			// 	}),
-			install: () =>
-				p.confirm({
-					message: 'Install dependencies?',
-					initialValue: true,
-				}),
-		},
-		{
-			onCancel: () => {
-				p.cancel('Operation cancelled.');
-				process.exit(0);
-			},
-		}
-	);
-
-	if (project.install) {
-		const s = p.spinner();
-		s.start(`Installing `);
-		// let selectedThemes = project.theme.split(',');
+	if (desktopDistro == "ubuntu"){await handleUbuntu()}
+	if (desktopDistro == "arch"){await handleArch()}
 
 
-		// await exec(`mkdir ${project.theme}`);
-		// await exec(`cd ${project.theme}`);
-		await setTimeout(2500);
-		s.stop('Installed Successfully!');
-	}
-
-	let nextSteps = `cd ${project.theme}        \n${project.install ? '' : 'pnpm install\n'}pnpm dev`;
-
-	p.note(nextSteps, 'Next steps.');
-
-	p.outro(`Problems? ${color.underline(color.cyan('https://example.com/issues'))}`);
 }
 
 
---
+++ src/scripts/ubuntu.ts
@@ -0,0 +1,58 @@
+import * as p from '@clack/prompts';
+import { setTimeout } from 'node:timers/promises';
+import color from 'picocolors';
+import { exec } from "../exec";
+import { os } from 'zx';
+import { execSync } from "child_process";
+
+async function handleUbuntu() {
+const project:any = await p.group(
+    {
+        theme: ({ results }) =>
+            p.multiselect({
+                    message: `Hey ${os.userInfo().username}, let's start with themes you would like.`,
+                    // initialValues: [],
+                    options: [
+                            { value: 'gruvbox', label: 'Gruvbox Dark', hint: 'recommended' },
+                            { value: 'everforest', label: 'Everforest Dark', hint: 'recommended' },
+                            { value: 'tokyonight', label: 'Tokyonight Storm' },
+                            { value: 'nightfox', label: 'Nightfox Dusk' },
+                            { value: 'kanagawa', label: 'Kanagawa' },
+                            { value: 'catppuccin', label: 'Catppuccin Mocha' },
+                            { value: 'material', label: 'Material Palenight' },
+                            { value: 'rosepine', label: 'Rosepine Main' },
+                        ],
+                    }),
+        install: () =>
+            p.confirm({
+                message: 'Install dependencies?',
+                initialValue: true,
+            }),
+    },
+    {
+        onCancel: () => {
+            p.cancel('Operation cancelled.');
+            process.exit(0);
+        },
+    }
+);
+
+if (project.install) {
+    const s = p.spinner();
+    s.start(`Installing `);
+    // let selectedThemes = project.theme.split(',');
+
+
+    // await exec(`mkdir ${project.theme}`);
+    // await exec(`cd ${project.theme}`);
+    await setTimeout(2500);
+    s.stop('Installed Successfully!');
+}
+
+let nextSteps = `cd ${project.theme}        \n${project.install ? '' : 'pnpm install\n'}pnpm dev`;
+
+p.note(nextSteps, 'Next steps.');
+
+p.outro(`Problems? ${color.underline(color.cyan('https://example.com/issues'))}`);
+}
+export {handleUbuntu}
